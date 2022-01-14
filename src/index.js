23aa9e7e2f937097e8540a8519c16598c9967c0d
skarlet <skarlet15m@gmail.com>
2022-01-13 18:30:18 -0600

    creacion de formulario y mostrar un alert

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 import validator from './validator.js';
 
 console.log(validator);
+
+
+(function(){
+    
+    const guardarNumero = document.getElementById('numero_tarjeta');
+    
+    const boton = document.getElementById("boton");
+    const guardar = function() {
+        alert(typeof(guardarNumero.value));
+        
+    };
+
+    boton.addEventListener("click", guardar);
+}())
+
+
+
