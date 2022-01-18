863f0539386732184a99cc6c6228dd5b80cf8880
skarlet <skarlet15m@gmail.com>
2022-01-17 18:56:39 -0600

    Aplicar el algoritmo de luhn al numero ingresado por el ususuario

--- src/validator.js
+++ src/validator.js
@@ -4,19 +4,32 @@ const validator = (numero) => {
   let array = [];
   
   for (let i = 0; i <= longitud-1; i++) {
-      array.push((numero[i]));
+      array.push(parseInt(numero[i]));
   };
   let array1= array.reverse();
-  
+  let mod = 0
+  let div = 0
 
   for (let i = 0; i <= longitud-1; i++) {
+
     if ((i % 2) == 0){
-      array1[i] = array1[i]*2
+      array1[i] = array1[i]*2;
+
+      if (array[i] >= 10) {
+        mod = array1[i] % 10;
+        div = Math.floor(array1[i] / 10);
+        array1[i] = mod + div;
+        
+      }
     }
   }
-  alert(array1);
   
+  let acum = 0
+  for ( let i = 0; i<=longitud-1; i++){
+    acum = acum + array1[i];
 
+  }
+  alert(acum);
 };
 
 export default validator;
