83366f26f68154b34525d99b0a329a60650ee4c7
skarlet <skarlet15m@gmail.com>
2022-01-17 18:13:27 -0600

    mover la logica desde el index.js al validator.js

--- src/index.js
+++ src/index.js
@@ -2,26 +2,16 @@ import validator from './validator.js';
 
 console.log(validator);
 
-
-(function(){
     
-    const guardarNumero = document.getElementById('numero_tarjeta');
-    
-    const boton = document.getElementById("boton");
-    const guardar = function() {
-        
-        let numero = guardarNumero.value;
-        let longitud = numero.length;
-        let array = [];
-        
-        for (let i = 0; i <= longitud-1; i++) {
-            array.push(parseInt(numero[i]));
-        };
-        alert(array)
-    };
+const guardarNumero = document.getElementById('numero_tarjeta');
+const boton = document.getElementById("boton");
 
-    boton.addEventListener("click", guardar);
-}())
+function guardar() {
+    validator(guardarNumero.value) 
+};
+
+boton.addEventListener("click", guardar);
+
 
 
 
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,22 @@
-const validator = {
-  // ...
+const validator = (numero) => {
+  
+  let longitud = numero.length;
+  let array = [];
+  
+  for (let i = 0; i <= longitud-1; i++) {
+      array.push((numero[i]));
+  };
+  let array1= array.reverse();
+  
+
+  for (let i = 0; i <= longitud-1; i++) {
+    if ((i % 2) == 0){
+      array1[i] = array1[i]*2
+    }
+  }
+  alert(array1);
+  
+
 };
 
 export default validator;
