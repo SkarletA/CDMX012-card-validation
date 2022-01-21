861edd4fee6323684e4ea2ebfe8ca583170fafac
skarlet <skarlet15m@gmail.com>
2022-01-20 22:21:10 -0600

    crear el mensaje de tarjeta valida e invalida y mostrar los datos ocultos

--- src/index.js
+++ src/index.js
@@ -5,10 +5,30 @@ import validator from './validator.js';
 //Guardar el numero ingresado en una constante  
 const guardarNumero = document.getElementById('numero_tarjeta');
 const boton = document.getElementById("boton");
+const parrafo =document.createElement("p");
+const mensaje= document.getElementById("mensaje");
+
+const numero_final = document.getElementById("numero_final");
+const nuevaTarjeta= document.createElement("p");
+
 
 function guardar() {
-    validator.isValid(guardarNumero.value)
+    if (validator.isValid(guardarNumero.value) == true){
+        let mensajeValido= document.createTextNode("Tarjeta Valida");
+        parrafo.appendChild(mensajeValido);
+        mensaje.appendChild(parrafo);
+    }
+    else {
+        let mensajeValido= document.createTextNode("Tarjeta Invalida");
+        parrafo.appendChild(mensajeValido);
+        mensaje.appendChild(parrafo);
+    }
+    const oculto= validator.maskify(guardarNumero.value);
+    nuevaTarjeta.innerHTML=`<p class="numero" id="numero_final">${oculto}</p>`;
+    numero_final.parentNode.replaceChild(nuevaTarjeta, numero_final);
+
 }
+
 
 boton.addEventListener("click", guardar);
 
--- src/validator.js
+++ src/validator.js
@@ -1,22 +1,22 @@
 const validator = {
-  isValid(numero){
+   isValid(numero){
     let longitud = numero.length;
     let array = [];
+    
     // Llenar el array vacio
-    for (let i = 0; i <= longitud-1; i++) {
+    for (let i = 0; i < longitud; i++) {
         array.push(parseInt(numero[i]));
     }
     
-    let array1= array.reverse(); //Invertir el array y guardarlo en una variable
+    let array1 = array.reverse(); //Invertir el array y guardarlo en una variable
     let mod = 0;
     let div = 0;
 
     //Recorrer el array
-    for (let i = 0; i <= longitud-1; i++) {
-    
+    for (let i = 0; i < longitud; i++) {
       if (((i+1) % 2) == 0){ //Saber si la posicion es par o impar partiendo desde 1
-        array1[i] = array1[i]*2;
-        if (array[i] >= 10) { //Si el numero es mayor a 10 
+        array1[i] = array1[i] * 2;
+        if (array[i] >= 10) { //Si el numero es mayor a 10 se descompone en resto(mod) y division(div)
           mod = array1[i] % 10;
           div = Math.floor(array1[i] / 10);
           array1[i] = mod + div;
@@ -27,24 +27,36 @@ const validator = {
     
     let acum = 0;
     // Sumatoria de todos los elementos que estam en el array
-    for ( let i = 0; i<=longitud-1; i++){
+    for ( let i = 0; i < longitud; i++){
       acum = acum + array1[i];
 
     }
     //Saber si la tarjeta es valida o invalida
-    if (acum%10==0){
-      alert("Tarjeta Valida");
+    if (acum % 10 == 0){
       return true;
       
     }
     else {
-      alert("Tarjeta Invalida");
       return false;
     }
 
   },
   
-  maskify(){}
+  maskify(numero){
+    //Ocultar los digitos de una tarjeta
+    let numeroOculto = [];
+    let longitud = numero.length;
+
+    for ( let i = 0; i < longitud; i++){
+      if (i < longitud-4){
+        numeroOculto.push("#")
+      }
+      else {
+        numeroOculto.push(numero[i])
+      }
+    }
+    return numeroOculto.join('');
+  }
 };
 
 export default validator;
